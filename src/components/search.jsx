a2c8840b2492022f8b7535ae288d24116d779335
sofia10 <makharashvilisofachka@gmail.com>
2023-04-26 17:34:04 +0400

    delicious app

---
+++ src/components/search.jsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import {useState} from 'react';
+import {FaSearch} from 'react-icons/fa';
+import {useNavigate} from 'react-router-dom';
+
+function Search(){
+
+    const [input, setInput]= useState('');
+    const navigate = useNavigate();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate('/searched/'+input);
+    }
+
+    return(
+        <FormStyle onSubmit={submitHandler}>
+            <div>
+                <FaSearch/>
+                <input 
+                    onChange={(e) => setInput(e.target.value)} 
+                    type="text" 
+                    value={input} 
+                />
+            </div>
+        </FormStyle >
+    )
+}
+
+const FormStyle = styled.form`
+    div{
+        width: 60%;
+        margin: auto;
+        position: relative;
+    }
+    input{
+        border: none;
+        background: linear-gradient(35deg, #494949, #313131);
+        font-size: 1.5rem;
+        color: #FFF;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 100%;
+    }
+    svg{
+        color: #FFF;
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+    }
+`
+
+export default Search;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,18 @@
+import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { Route, Routes } from 'react-router-dom';
+import Searched from "./Searched";
+import Recipe from "./Recipe";
+
+function Pages(){
+    return(
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cuisine/:type" element={<Cuisine/>}></Route>
+            <Route path="/searched/:search" element={<Searched/>}></Route>
+            <Route path="/recipe/:name" element={<Recipe/>}></Route>
+        </Routes>
+    )
+}
+
+export default Pages;
